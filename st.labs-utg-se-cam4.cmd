a325e03c3878be99aa0701f02db6cd1acb4f3091
Wayne Lewis <wayne.lewis@esss.se>
2019-02-08 11:40:43 +0100

    ENH: Use E3 autosave startup

--- st.labs-utg-se-cam4.cmd
+++ st.labs-utg-se-cam4.cmd
@@ -1,39 +1,33 @@
-#  require ADPointGrey,2.7.0
-  require ADPointGrey,develop
-  require ADPluginCalib,0.0.1
-  require busy,1.7.0
-  require sequencer,2.1.21
-  require sscan,1339922
-  require calc,3.7.0
-  require autosave,5.9.0
+require ADPointGrey,2.7.0-dev
+#  require ADPointGrey,develop
+require ADPluginCalib,0.0.1
+require busy,1.7.0+
+require sequencer,2.1.21+
+require sscan,1339922+
+require calc,3.7.0+
+require autosave,5.9.0+
 
-  epicsEnvSet("IOC",		"iocPointGrey")
-  epicsEnvSet("TOP",		".")
-  epicsEnvSet("ADPOINTGREY",	"/home/iocuser/e3/e3-ADPointGrey/ADPointGrey")
-# epicsEnvSet("ADPOINTGREY",	"/home/utgard/epics_env/epics-modules/areaDetector/ADPointGrey/iocs/pointGreyIOC/../..")
-# epicsEnvSet("ADPLUGINEDGE",	"/home/iocuser/e3/e3-ADPluginEdge/ADPluginEdge")
-  epicsEnvSet("ADPLUGINCALIB",	"/home/iocuser/e3/e3-ADPluginCalib/ADPluginCalib")
-# epicsEnvSet("AREA_DETECTOR",	"/home/utgard/epics_env/epics-modules/areaDetector")
-# epicsEnvSet("EPICS_BASE",	"/home/utgard/epics_env/epics-base")
-# epicsEnvSet("EPICS_MODULES",	"/home/utgard/epics_env/epics-modules")
-# epicsEnvSet("ASYN",		"/home/utgard/epics_env/epics-modules/asyn")
-# epicsEnvSet("ADSUPPORT",	"/home/utgard/epics_env/epics-modules/areaDetector/ADSupport")
-# epicsEnvSet("ADCORE",		"/home/utgard/epics_env/epics-modules/areaDetector/ADCore")
-  epicsEnvSet("ADCORE",		"/home/iocuser/e3/e3-ADCore/ADCore")
-# epicsEnvSet("AUTOSAVE",	"/home/utgard/epics_env/epics-modules/autosave")
-  epicsEnvSet("AUTOSAVE",	"/home/iocuser/e3/e3-autosave/autosave")
-# epicsEnvSet("BUSY",		"/home/utgard/epics_env/epics-modules/busy")
-# epicsEnvSet("CALC",		"/home/utgard/epics_env/epics-modules/calc")
-  epicsEnvSet("CALC",		"/home/iocuser/e3/e3-calc")
-# epicsEnvSet("SNCSEQ",		"/home/utgard/epics_env/epics-modules/seq")
-# epicsEnvSet("SSCAN",		"/home/utgard/epics_env/epics-modules/sscan")
+epicsEnvSet("CAMERA_ID", "18457601")
+epicsEnvSet("AREA", "labs-utg-se")
+epicsEnvSet("DEVICE", "cam4")
 
+epicsEnvSet("PREFIX_PV", "$(AREA):$(DEVICE):")
+epicsEnvSet("IOCNAME", "$(AREA)-$(DEVICE)")
+epicsEnvSet("CAM", "")
+epicsEnvSet("IMAGE", "image1:")
 
+epicsEnvSet("IOC", "iocPointGrey")
+epicsEnvSet("TOP", ".")
+epicsEnvSet("AS_TOP", "/epics/autosave")
+
+epicsEnvSet("ADPOINTGREY", "/home/iocuser/e3/e3-ADPointGrey/ADPointGrey")
+epicsEnvSet("ADCORE", "/home/iocuser/e3/e3-ADCore/ADCore")
+epicsEnvSet("AUTOSAVE", "")
 
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","64000000")
 
 ### The port name for the detector
-epicsEnvSet("PORT",   "PG1")
+epicsEnvSet("PORT",   "PG")
 ### Really large queue so we can stream to disk at full camera speed
 epicsEnvSet("QSIZE",  "2000")   
 ### The maximim image width; used for row profiles in the NDPluginStats plugin
@@ -52,84 +46,47 @@ epicsEnvSet("NELEMENTS", "12582912")
 #########################   camera 1 #######################################################################################################################################
 ### pointGreyConfig(const char *portName, int cameraId, int traceMask, int memoryChannel,
 ###                 int maxBuffers, size_t maxMemory, int priority, int stackSize)
-epicsEnvSet("CAMERA_ID", "17280203")
-epicsEnvSet("PREFIX", "PG4:")
-pointGreyConfig("$(PORT)", $(CAMERA_ID), 0x1, 0)
+pointGreyConfig("$(PORT)", "$(CAMERA_ID)", 0x1, 0)
 
 asynSetTraceIOMask($(PORT), 0, 2)
 ###asynSetTraceMask($(PORT), 0, 0xFF)
 ###asynSetTraceFile($(PORT), 0, "asynTrace.out")
 ###asynSetTraceInfoMask($(PORT), 0, 0xf)
 
-dbLoadRecords("pointGrey.db", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("pointGreyPG1-ess.db")
+dbLoadRecords("pointGrey.db", "P=$(PREFIX_PV),R=$(CAM),PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("pointGrey-ess.db", "P=$(PREFIX_PV),R=$(CAM),PORT=$(PORT)")
 
 ### Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, $(PORT), 0, 0)
 ### Use this line for 8-bit data only
-###dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int8,FTVL=CHAR,NELEMENTS=$(NELEMENTS)")
+###dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX_PV),R=$(IMAGE),PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int8,FTVL=CHAR,NELEMENTS=$(NELEMENTS)")
 ### Use this line for 8-bit or 16-bit data
-dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=$(NELEMENTS)")
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX_PV),R=$(IMAGE),PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=$(NELEMENTS)")
 
 ### Load all other plugins using commonPlugins.cmd
 
-epicsEnvSet("PREFIX", "${PREFIX}")
-epicsEnvSet("PORT"  , "${PORT}")
-#< /home/iocuser/e3/e3-ADPointGrey/cmds/adpointgrey_commonPlugins.cmd 
-#set_requestfile_path("$(ADPOINTGREY)/pointGreyApp/Db")
-
-NDCalibConfigure("CALIB1", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0)
-dbLoadRecords("$(ADPLUGINCALIB)/calibApp/Db/NDCalib.db", "P=$(PREFIX),R=Calib1:,PORT=CALIB1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
-
-#< $(ADCORE)/iocBoot/EXAMPLE_commonPlugins.cmd
+epicsEnvSet(PREFIX, "$(PREFIX_PV)")
 < $(ADCORE)/../cmds/commonPlugins.cmd
 
-#############################################################################################
-## autosave/restore machinery
-#save_restoreSet_Debug(0)
-#save_restoreSet_IncompleteSetsOk(1)
-#save_restoreSet_DatedBackupFiles(1)
-# 
-#set_savefile_path(   "$(ADPOINTGREY)/autosave","/sav")
-#set_requestfile_path("$(ADPOINTGREY)/autosave","/req")
-# 
-## set_pass0_restoreFile("info_positions.sav")
-#set_pass0_restoreFile("info_settings.sav")
-#set_pass1_restoreFile("info_settings.sav")
-# 
-
-set_requestfile_path("$(ADPOINTGREY)/pointGreyApp/Db")
-set_requestfile_path("$(ADPOINTGREY)/iocs/pointGreyIOC/iocBoot/iocPointGrey")
-set_requestfile_path("$(ADPOINTGREY)/../cmds")
-set_requestfile_path("$(ADCORE)/../cmds")
+iocshLoad("$(autosave_DIR)/autosave.iocsh")
 
 iocInit()
-#
-## more autosave/restore machinery
-#cd autosave/req
-#makeAutosaveFiles()
-#cd ../..
-#create_monitor_set("info_positions.req", 5 , "")
-#create_monitor_set("info_settings.req", 15 , "")
-create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
-
-#############################################################################################
 
 ## Wait for enum callbacks to complete
 epicsThreadSleep(5.0)
 
 ## Records with dynamic enums need to be processed again because the enum values are not available during iocInit.  
-dbpf("$(PREFIX)cam1:Format7Mode.PROC", "1")
-dbpf("$(PREFIX)cam1:PixelFormat.PROC", "1")
+dbpf("$(PREFIX_PV)$(CAM)Format7Mode.PROC", "1")
+dbpf("$(PREFIX_PV)$(CAM)PixelFormat.PROC", "1")
 
 ## Wait for callbacks on the property limits (DRVL, DRVH) to complete
 epicsThreadSleep(5.0)
 
 ## Records that depend on the state of the dynamic enum records or property limits also need to be processed again
 ## Other property records may need to be added to this list
-dbpf("$(PREFIX)cam1:FrameRate.PROC", "1")
-dbpf("$(PREFIX)cam1:FrameRateValAbs.PROC", "1")
-dbpf("$(PREFIX)cam1:AcquireTime.PROC", "1")
-dbpf("$(PREFIX)cam1:FrameRateValAbs_RBV", "3");
+dbpf("$(PREFIX_PV)$(CAM)FrameRate.PROC", "1")
+dbpf("$(PREFIX_PV)$(CAM)FrameRateValAbs.PROC", "1")
+dbpf("$(PREFIX_PV)$(CAM)AcquireTime.PROC", "1")
+dbpf("$(PREFIX_PV)$(CAM)FrameRateValAbs_RBV", "3");
 
 
